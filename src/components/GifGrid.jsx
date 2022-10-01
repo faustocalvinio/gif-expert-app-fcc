0411080d388c5a54015dce8e42b6e8766421d986
faustocalvinio <faustocalvino@outlook.com>
2022-10-02 00:38:13 +0200

    input change and category center

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -11,7 +11,7 @@ export const GifGrid = ({ category }) => {
 
     return (
         <>
-            <h3> { category } </h3>  
+            <h3 className="category-title"> { category } </h3>  
             { 
                 isLoading && ( <h2>Cargando...</h2> )
                 
