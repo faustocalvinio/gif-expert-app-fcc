80fd3ccdabb1704a6f12e7efdf5b070beacde1e3
Fausto Calviño <faustocalvino@outlook.com>
2023-10-20 12:30:49 +0200

    feat: removed unnecesary code

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 
 export const AddCategory = ({ onNewCategory }) => {
 
-    const [inputValue, setInputValue] = useState()
+    const [inputValue, setInputValue] = useState();
 
     const onInputChange=(event)=>{
         setInputValue( event.target.value )
-    }
+    };
     const onSubmit=(event)=>{
         event.preventDefault();
         if( inputValue.trim().length<=1 )return;
         onNewCategory( inputValue.trim() )
         setInputValue('');
-    }
+    };
 
     return (
         <>        
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,17 +1,15 @@
 const API_KEY=import.meta.env.VITE_GIF_API_KEY;
 
 export const getGifs=async( category )=>{
-    console.log(API_KEY);
     const url=`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${ category }&limit=10`;
 
     const resp = await fetch(url);
     const {data}= await resp.json();
 
-
     const gifs=data.map( img =>({
         id: img.id,
         title:img.title,
         url:img.images.downsized_medium.url,
-    }))
+    }));
     return gifs;
 }
