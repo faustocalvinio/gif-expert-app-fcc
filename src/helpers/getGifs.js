d435b5343d0b8079e72776146582ac5fad379f60
Fausto Calviño <faustocalvino@outlook.com>
2023-10-20 12:26:06 +0200

    feat: .env.template file

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,8 @@
+const API_KEY=import.meta.env.VITE_GIF_API_KEY;
+
 export const getGifs=async( category )=>{
-    const url=`https://api.giphy.com/v1/gifs/search?api_key=PznE4m6RH8CIL1nrOoNkEbI9cvhIMiX6&q=${ category }&limit=10`;
+    console.log(API_KEY);
+    const url=`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${ category }&limit=10`;
 
     const resp = await fetch(url);
     const {data}= await resp.json();
